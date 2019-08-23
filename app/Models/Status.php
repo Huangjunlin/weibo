ea5a8a4e8dc5bf961cb745650f7619d552142eec
king <junlinhuang1@gmail.com>
2019-08-23 18:57:27 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     /**
      * 建立一对多的关系,指明一个用户拥有多条微博
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
